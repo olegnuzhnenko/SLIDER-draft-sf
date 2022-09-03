b1aef8df2e1ba38e0affd136f8ecd9da2ff6767e
olegnuzhnenko <82514158+olegnuzhnenko@users.noreply.github.com>
2022-09-03 18:19:57 +0200

    Add files via upload

---
+++ SLIDER Oleg Nuznenko/app.js
@@ -0,0 +1,5 @@
+const prev = document.getElementsByClassName('btn-prev'),
+        next = document.getElementsByClassName('btn-next'),
+        slides = document.querySelectorAll('.slide'),
+        dots = document.querySelectorAll('.dot')
+
